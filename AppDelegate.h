a7b2a432a9689bdb40ff6724fe441c625ab5eb78
Nicholas Casamento <nick.casamento@gmail.com>
2015-05-12 14:57:58 -0400

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Basic Calculator
+//
+//  Created by Nicholas Casamento on 3/16/15.
+//  Copyright (c) 2015 Nicholas Casamento. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
